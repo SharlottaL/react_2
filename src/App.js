cc03118e7e3c36932cc50198aa4016e5d8bfe79b
SharlottaL <botanichkagigantika@gmail.com>
2026-02-13 15:18:50 +0300

    TODO: removePost()

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import Hello from './Hello.js';
 import Length  from './Lenth.js';
 import Range from './Range.js';
 import Form from './Form.js';
+import Posts from './posts/Posts.js';
+
+ let posts = [
+    {id:'1', title:'C++ p', content:'C++ proc'},
+    {id:'2', title:'C++ OOP', content:'C++ OOP'},
+    {id:'3', title:'WD', content:'Windows Desktop'},
+  ]
+
+function removePost(id)
+{
+posts = posts.filter(post => post.id !== id);
+}
 function App() {
+ 
   return (
     <div className="App">
-     <Header/>
+     {/* <Header/>
 <Hello/>
 <Length/>
 <Range/>
-<Form/>
+<Form/> */}
+<Posts posts={posts}/>
     </div>
   );
 }
